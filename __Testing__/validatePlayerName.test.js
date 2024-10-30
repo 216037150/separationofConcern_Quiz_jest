fe39547a331de65735c0a188dd572495c1b58981
216037150 <siyabongazungu95@gmail.com>
2024-10-30 19:12:11 -0400

    Added verbose in Json package

--- __Testing__/validatePlayerName.test.js
+++ __Testing__/validatePlayerName.test.js
@@ -4,10 +4,10 @@ import jest from 'jest-mock';
 describe('playerName validation with error throwing', () => {
     test('should throw errors', () => {
         const emptName = '';
-        const digName = '123';
+        const digitName = '123';
         const speciName = '!@'
         expect(() => validatePlayerName(emptName)).toThrow('Please enter your name');
-        expect(() => validatePlayerName(digName)).toThrow('Invalid input: Please enter a valid name, not a number.');
+        expect(() => validatePlayerName(digitName)).toThrow('Invalid input: Please enter a valid name, not a number.');
         expect(() => validatePlayerName(speciName)).toThrow('Invalid input: Please do not use special characters.');
     });
 });
